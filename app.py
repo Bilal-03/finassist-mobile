ceeb935d61aa39529eeda9dc5e5399f54b7bf220
Bilal <bilal@Bilals-MacBook-Pro.local>
2026-02-23 15:24:55 +0530

    Fix app.py

--- app.py
+++ app.py
@@ -789,5 +789,5 @@ def mf_detail(scheme_code):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(debug=False)
 
